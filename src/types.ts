0f173719d47a5ab715d1c1ee22ad46d1abd22683
Owan Hunte <owanh@hotmail.com>
2020-07-04 13:57:56 -0400

    first commit

---
+++ src/types.ts
@@ -0,0 +1,81 @@
+export type RootTabsParamList = {
+  Dashboard: undefined;
+  News: undefined;
+  Immigration: undefined;
+  Stats: undefined;
+};
+
+export type RootDrawerParamList = {
+  Dashboard: undefined;
+  News: undefined;
+  Immigration: undefined;
+  Stats: undefined;
+  Hotline: undefined;
+  ReportSelf: undefined;
+};
+
+//------------------------ Data types for COVID-19 API Statistics ------------------------------//
+
+export interface BaseStats {
+  status: string;
+  cases: number;
+  todayCases: number;
+  deaths: number;
+  todayDeaths: number;
+  active: number;
+  tests: number;
+  testsPerOneMillion?: number;
+  casesPerOneMillion?: number;
+  deathsPerOneMillion?: number;
+}
+
+export interface BaseTotalStats extends BaseStats {
+  critical: number;
+  recovered: number;
+  updated: number;
+}
+
+export interface BaseCountryStats extends BaseStats {
+  female?: number;
+  male?: number;
+  critical?: number;
+  recovered?: number;
+  updated?: number;
+}
+
+export interface TotalStats extends BaseTotalStats {
+  type: string;
+  affectedCountries?: number;
+  countriesTracked?: {
+    country: string;
+    iso2: string;
+    iso3: string;
+  }[];
+}
+
+export interface CountryInfo {
+  iso2: string;
+  iso3: string;
+  lat?: number;
+  long?: number;
+}
+
+export interface CountryStats extends BaseCountryStats {
+  country: string;
+  countryCode: string;
+  countryInfo: CountryInfo;
+}
+
+export interface CountryStatsSearchParams {
+  countryCode?: string;
+  status?: string;
+}
+
+//------------------------ Data types for the News feed ------------------------------//
+export type NewsFeedItem = {
+  title: string;
+  urlSlug: string;
+  image: string;
+  articleLink: string;
+  publishDate: string;
+};
